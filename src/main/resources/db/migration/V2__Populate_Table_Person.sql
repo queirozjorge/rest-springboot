976d96e09cfc2e2a944725ac1b075d321ec17464
Jorge <jorge-rick@hotmail.com>
2023-03-10 17:14:24 -0300

    Add db migrate usgin flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+    (1, 'Cotia - SP', 'Jorge', 'Male', 'Queiroz'),
+    (3, 'Cotia - SP', 'Jorge Ricardo', 'Male', 'Gomes de Andrade Queiroz'),
+    (5, 'Campinas - SP', 'Leonardo', 'Male', 'Zago'),
+    (6, 'Cuiabá - MT', 'Igor', 'Male', 'Arruda'),
+    (7, 'Cuiabá - MT', 'Igor', 'Male', 'Arruda');
